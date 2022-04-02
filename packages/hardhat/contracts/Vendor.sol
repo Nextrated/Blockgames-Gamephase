260a9b6ff260673d45a7d70ee6c4d073ba3e228b
Denedo Pearl <pearliboy1@gmail.com>
2022-04-02 16:49:11 +0100

    third commit

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -6,18 +6,53 @@ import "./YourToken.sol";
 
 contract Vendor is Ownable {
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
-
   YourToken public yourToken;
+
+  uint256 public constant tokensPerEth = 100;
+
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  
 
   constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
   }
 
   // ToDo: create a payable buyTokens() function:
+   function buyTokens() public payable {
+    require(msg.value > 0, 'ETH used to buy token must be greater than 0');
+
+    uint256 tokenToBuy = msg.value * tokensPerEth;
+
+    yourToken.transfer(msg.sender, tokenToBuy);
+
+    emit BuyTokens(msg.sender, msg.value, tokenToBuy);
+
+  }
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
 
+  function withdraw() public onlyOwner {
+  uint256 ethToWithdraw = address(this).balance;
+  require(ethToWithdraw > 0, 'No ETH to withdraw');
+
+  payable(msg.sender).transfer(ethToWithdraw);
+}
+
   // ToDo: create a sellTokens() function:
+
+  function sellTokens(uint256 tokenToSell) public {
+  require(tokenToSell > 0, 'You need to have at least some tokens');
+
+  // Calculate the needed ETH amount
+  uint256 ethSold = tokenToSell / tokensPerEth;
+
+  require(address(this).balance >= ethSold, 'Not enough ETH to buy from Vendor');
+
+  // Transfer Token from user to Vendor contract
+  yourToken.transferFrom(msg.sender, address(this), tokenToSell);
+
+  payable(msg.sender).transfer(ethSold);
+
+}
 
 }
